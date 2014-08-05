47fde2f4c1126fe1e1ecc1a9b9e0d71c176a745f
phataim <317012583@qq.com>
2014-08-05 19:57:25 +0800

    Update Ys.jsonp.js

--- Ys.jsonp.js
+++ Ys.jsonp.js
@@ -1,31 +1,35 @@
-Ys.jsonp = function(options){
+;(function(){
+	if (typeof(window.Ys) === 'undefined') window.Ys = {};
+    Ys.jsonp = function(options){
 
-    Ys.jsonp.id = 0;
+        Ys.jsonp.id = 0;
 
-    var script = document.createElement('script');
+        var script = document.createElement('script');
 
-    if(/\?/.test(options.url)){
-        script.src = options.url + '&callback=' + options.callback;
-    } else {
-        script.src = options.url + '?callback=' + options.callback;
-    }
+        if(/\?/.test(options.url)){
+            script.src = options.url + '&callback=' + options.callback;
+        } else {
+            script.src = options.url + '?callback=' + options.callback;
+        }
  
-    var tmpData = null;
+        var tmpData = null;
 
-    window[options.callback] = function(data){tmpData = data};
+        window[options.callback] = function(data){tmpData = data};
 
-    script.onload = script.onreadystatechange = function(){
-        if(typeof(this.readyState) === "undefined" || this.readyState === "loaded" || this.readyState === "complete" ){
+        script.onload = script.onreadystatechange = function(){
+            if(typeof(this.readyState) === "undefined" || this.readyState === "loaded" || this.readyState === "complete" ){
             
-            options.onSuccess(tmpData);
-            tmpData = null;
-            delete window[options.callback];
-            var tmp = document.getElementById('jsonpScript' + Ys.jsonp.id);
-            tmp.parentNode.removeChild(tmp);
-            Ys.jsonp.id++;
-        } 
-    }
-    script.id = 'jsonpScript' + Ys.jsonp.id;
-    Ys('#content')[0].appendChild(script);   
+                options.onSuccess(tmpData);
+                tmpData = null;
+                delete window[options.callback];
+                var tmp = document.getElementById('jsonpScript' + Ys.jsonp.id);
+                tmp.parentNode.removeChild(tmp);
+                Ys.jsonp.id++;
+            } 
+        }
+        script.id = 'jsonpScript' + Ys.jsonp.id;
+        Ys('#content')[0].appendChild(script);   
 
-}
+    }
+    
+})(window);
